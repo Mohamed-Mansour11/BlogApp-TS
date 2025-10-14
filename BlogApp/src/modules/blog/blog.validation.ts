ef2473c70021747101a902e5f4e9121bcc081b87
Mohamed-Mansour11 <mohamed.mansour291991@gmail.com>
2025-10-15 01:32:03 +0300

    Update blog.validation.ts

--- BlogApp/src/modules/blog/blog.validation.ts
+++ BlogApp/src/modules/blog/blog.validation.ts
@@ -1,47 +1,30 @@
-import { NextFunction, Response } from "express";
-import { IAuthReq } from "../../middleware/auth.middleware";
-import BlogModel from "../../DB/model/blog.model";
-import { successResponse } from "../../utils/response/success.response";
+import Joi from "joi";
 
-class BlogService {
-    constructor(private blogModel = BlogModel) { }
+export const createBlog = Joi.object({
+    body: Joi.object({
+        title: Joi.string().min(2).max(100).required(),
+        description: Joi.string().min(10).required()
+    }),
+    params: Joi.any(),
+    query: Joi.any()
+});
 
-    public create = async (req: IAuthReq, res: Response) => {
-        const { title, description } = req.body;
-        const blog = await this.blogModel.create({ title, description, createdBy: req.user._id });
-        return successResponse(res, "Blog created", 201, { blog });
-    }
+export const updateBlog = Joi.object({
+    body: Joi.object({
+        title: Joi.string().min(2).max(100),
+        description: Joi.string().min(10)
+    }).or('title', 'description'),
+    params: Joi.object({
+        id: Joi.string().hex().length(24).required()
+    }),
+    query: Joi.any()
+});
 
-    public list = async (req: IAuthReq, res: Response) => {
-        const blogs = await this.blogModel.find({}).populate({
-            path: 'createdBy',
-            select: 'username -_id'
-        });
-        return successResponse(res, "Blogs list", 200, { blogs });
-    }
 
-    public update = async (req: IAuthReq, res: Response, next: NextFunction) => {
-        const { id } = req.params;
-        const { title, description } = req.body;
-        const blog = await this.blogModel.findOneAndUpdate(
-            { _id: id, createdBy: req.user._id }, 
-            { title, description },
-            { new: true }
-        );
-        if (!blog) {
-            return next(new Error("Blog not found or you are not authorized to edit it", { cause: 404 }));
-        }
-        return successResponse(res, "Blog updated successfully", 200, { blog });
-    }
-
-    public delete = async (req: IAuthReq, res: Response, next: NextFunction) => {
-        const { id } = req.params;
-        const blog = await this.blogModel.findOneAndDelete({ _id: id, createdBy: req.user._id });
-        if (!blog) {
-            return next(new Error("Blog not found or you are not authorized to delete it", { cause: 404 }));
-        }
-        return successResponse(res, "Blog deleted successfully", 200);
-    }
-}
-
-export default new BlogService();
+export const deleteBlog = Joi.object({
+    params: Joi.object({
+        id: Joi.string().hex().length(24).required()
+    }),
+    body: Joi.any(),
+    query: Joi.any()
+});
